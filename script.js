62d2cb46b6da9d8aa4b6cd33937287a02fd61b84
muthuraj <muthuraj1695@gmail.com>
2023-11-29 12:50:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const sum=function (arr){
+    let value=0;
+    for(let i=0;i<arr.length;i++){
+        value=value+arr[i];
+    }
+    console.log(value)
+}
+sum([9,9,8,7,8]);
+
+
+// swap two number
+
+
+let a=50 , b=90;
+
+[a,b]= [b,a];
+
+ console.log(a,b);
+
+
